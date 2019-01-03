ad36612b30389c0f19a347452be7768ad8a93ec3
Grafiters <bayugrafit@gmail.com>
2019-01-03 14:11:42 +0700

    make controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/' => 'items#index'
+  get '/login' => 'users#index'
+  get '/register' => 'registers#index'
+
+  post '/user' => 'users#create'
+  post '/session' => 'sessions#create'
+
 end
