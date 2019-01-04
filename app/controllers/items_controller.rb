9ccdc7a0bcac7e8cb3383c5fffc32a2be3b29b63
Grafiters <bayugrafit@gmail.com>
2019-01-05 01:22:23 +0700

    controller item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,33 @@
 class ItemsController < ApplicationController
+
+    before_action :find_item,   only: [:show, :edit, :update, :destroy]
+
     def index
         
     end
     def show
         
     end
+    def new
+        @item = Item.new
+    end
+    def create
+        @item = Items.new(items_params)
+        if @item.save
+            flash[:add_items_success] = ['items has been created']
+            redirect_to '/'
+        else
+            flash[:add_items_errors] = ['items not create, please fill the correctly form']
+            redirect_to '/add-items'
+        end
+    end
     
-    
+    private
+        def items_params
+            params.require(:item).permit(:title, :description, :price, :image)
+        end
+        def find_item
+            @item = Item.find(params[:id])
+        end
+                
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'items#index'
   get '/login' => 'users#index'
-  get '/register' => 'registers#index'  
+  get '/register' => 'registers#index'
+  get '/add-items' => 'items#new'
 
   post '/users' => 'users#create'
   post '/regis' => 'registers#create'
